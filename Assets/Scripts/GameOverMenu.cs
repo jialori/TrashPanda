52a02169c64b8200e9c062e7baf0b4bd7601dd5d
Gabriel <neoco@techie.com>
2019-09-23 18:00:22 -0400

    added a game over screen

---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public string sceneName = "MainScene";
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
