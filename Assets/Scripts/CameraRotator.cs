0281e5e71249920fb6c30b6bf7977c674b51ce0c
Gabriel <neoco@techie.com>
2019-10-03 20:29:36 -0400

    implemented camera rotation

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 10.0f;
+    private float sensitivityY = 10.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        currentY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
--- Assets/Scripts/RaccoonController.cs
+++ Assets/Scripts/RaccoonController.cs
@@ -10,12 +10,17 @@ public class RaccoonController : MonoBehaviour
     [SerializeField] private int maxFood = 10;
 
     private Rigidbody rb;
+    public float jumpVelocity = 50f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Debug.Log(rb);
     }
 
+    
 
     void FixedUpdate()
     {
@@ -23,6 +28,24 @@ public class RaccoonController : MonoBehaviour
         float moveVertical = speed * Input.GetAxis("Vertical");
         Vector3 movePlayer = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.velocity = movePlayer;
+        
+        
+        // Code from jump script
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("jump press");
+            rb.velocity = Vector3.up * jumpVelocity;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        
+        
     }
 
     public int GetFood()
