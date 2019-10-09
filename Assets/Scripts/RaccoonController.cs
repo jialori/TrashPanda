8320aa8df677ccbb7d19d15f24b8a5df86e07cca
Ellen (Yufei) Chen <chenellen007@gmail.com>
2019-10-09 16:46:01 -0400

    Modified some values

--- Assets/Scripts/RaccoonController.cs
+++ Assets/Scripts/RaccoonController.cs
@@ -4,210 +4,214 @@ using UnityEngine;
 
 public class RaccoonController : MonoBehaviour
 {
-    [SerializeField] private AudioClip itemCollectSound;
-    [SerializeField] private bool useController = true;
-    [SerializeField] private int food = 0;
-    [SerializeField] private int maxFood = 10;
+  [SerializeField] private AudioClip itemCollectSound;
+  [SerializeField] private bool useController = true;
+  [SerializeField] private int food = 0;
+  [SerializeField] private int maxFood = 10;
 
-    [SerializeField] private Transform cam;
+  [SerializeField] private Transform cam;
 
-    private Vector3 movementVector;
+  private Vector3 movementVector;
 
-    private CharacterController characterController;
+  private CharacterController characterController;
 
-    private float movementSpeed = 10;
-    private float jumpPower = 15;
-    private float gravity = 40;
+  [SerializeField] private float movementSpeed = 10;
+  private float jumpPower = 15;
+  private float gravity = 40;
 
-    // First add a Layer "Breakable"/"Knockable" to all breakable and knockable objects in Unity Engine 
-    private float raycastPaddedDist;
-    private float raycastPadding = 0.2f;
-    private int radiusStep = 36; // how many degree does each raycast check skips, dcrease if want more accuracy
-    private string breakableMaskName = "Breakable";
-    private string knockableMaskName = "Knockable";
-    private int breakableMask;
-    private int knockableMask;
-    private float pushPower = 12;
+  // First add a Layer "Breakable"/"Knockable" to all breakable and knockable objects in Unity Engine 
+  private float raycastPaddedDist;
+  private float raycastPadding = 0.2f;
+  private int radiusStep = 36; // how many degree does each raycast check skips, dcrease if want more accuracy
+  private string breakableMaskName = "Breakable";
+  private string knockableMaskName = "Knockable";
+  private int breakableMask;
+  private int knockableMask;
+  private float pushPower = 12;
 
-    void Start()
+  void Start()
+  {
+    characterController = GetComponent<CharacterController>();
+
+    // Raycasy for breakable and knockable objects
+    raycastPaddedDist = characterController.radius + raycastPadding;
+    breakableMask = 1 << LayerMask.NameToLayer(breakableMaskName);
+    knockableMask = 1 << LayerMask.NameToLayer(knockableMaskName);
+  }
+
+  void Update()
+  {
+    // Adjust movement for camera angle
+    var camForward = cam.forward;
+    var camRight = cam.right;
+    camForward.y = 0;
+    camRight.y = 0;
+    camForward = camForward.normalized;
+    camRight = camRight.normalized;
+    var prevY = movementVector.y;
+
+    // movement
+    movementVector = (camForward * GetYAxis() + camRight * GetXAxis()) * movementSpeed;
+
+    // Jump
+    if (characterController.isGrounded)
     {
-        characterController = GetComponent<CharacterController>();
+      movementVector.y = 0;
 
-        // Raycasy for breakable and knockable objects
-        raycastPaddedDist = characterController.radius + raycastPadding; 
-        breakableMask = 1 << LayerMask.NameToLayer(breakableMaskName);
-        knockableMask = 1 << LayerMask.NameToLayer(knockableMaskName);
+      if (GetJump())
+      {
+        movementVector.y = jumpPower;
+      }
+
+    }
+    else
+    {
+      movementVector.y = prevY;
     }
 
-    void Update()
-    {        
-        // Adjust movement for camera angle
-        var camForward = cam.forward;
-        var camRight = cam.right;
-        camForward.y = 0;
-        camRight.y = 0;
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-        var prevY = movementVector.y;
+    movementVector.y -= gravity * Time.deltaTime;
+    Debug.Log("movementVector = " + movementVector);
+    characterController.Move(movementVector * Time.deltaTime);
 
-        // movement
-        movementVector = (camForward * GetYAxis() + camRight * GetXAxis()) * movementSpeed;
-        
-        // Jump
-        if (characterController.isGrounded)
+    // Breakable objects
+    RaycastHit hit;
+    if (Input.GetButtonDown("B"))
+    {
+      // Bottom of controller. Slightly above ground so it doesn't bump into slanted platforms.
+      Vector3 p1 = transform.position + Vector3.up * 0.01f;
+      Vector3 p2 = p1 + Vector3.up * characterController.height;
+      // Check around the character in 360 degree
+      for (int i = 0; i < 360; i += radiusStep)
+      {
+        // Check if anything with the platform layer touches this object
+        if (Physics.CapsuleCast(p1, p2, 0, new Vector3(Mathf.Cos(i), 0, Mathf.Sin(i)), out hit, raycastPaddedDist, breakableMask))
         {
-            movementVector.y = 0;
-
-            if (GetJump())
-            {
-                movementVector.y = jumpPower;
-            }
-
-        } else {
-            movementVector.y = prevY;
+          Breakable breakable = hit.collider.gameObject.GetComponent<Breakable>() as Breakable;
+          if (breakable != null)
+          {
+            breakable.trigger();
+          }
         }
-
-        movementVector.y -= gravity * Time.deltaTime;
-        Debug.Log("movementVector = " + movementVector);
-        characterController.Move(movementVector * Time.deltaTime);
-
-        // Breakable objects
-        RaycastHit hit;
-        if (Input.GetButtonDown("B")) {
-            // Bottom of controller. Slightly above ground so it doesn't bump into slanted platforms.
-            Vector3 p1 = transform.position + Vector3.up * 0.01f;
-            Vector3 p2 = p1 + Vector3.up * characterController.height;
-            // Check around the character in 360 degree
-             for(int i=0; i<360; i+= radiusStep){
-                // Check if anything with the platform layer touches this object
-                if (Physics.CapsuleCast(p1, p2, 0, new Vector3(Mathf.Cos(i), 0, Mathf.Sin(i)), out hit, raycastPaddedDist, breakableMask)){
-                    Breakable breakable = hit.collider.gameObject.GetComponent<Breakable>() as Breakable;
-                    if (breakable != null) {
-                        breakable.trigger();
-                    }
-                }
-            }
-        }
-
-        //[Optional] Check the players feet and push them up if something clips through their feet.
-        //(Useful for vertical moving platforms)
-        // if (Physics.Raycast(transform.position+Vector3.up, -Vector3.up, out hit, 1, knockableMask)){
-        //     characterController.Move(Vector3.up * (1-hit.distance));
-        // }
-
+      }
     }
 
-    private float GetXAxis() 
+    //[Optional] Check the players feet and push them up if something clips through their feet.
+    //(Useful for vertical moving platforms)
+    // if (Physics.Raycast(transform.position+Vector3.up, -Vector3.up, out hit, 1, knockableMask)){
+    //     characterController.Move(Vector3.up * (1-hit.distance));
+    // }
+
+  }
+
+  private float GetXAxis()
+  {
+    if (useController)
     {
-        if (useController) 
-        {
-            return Input.GetAxis("LeftJoystickX");
-        } 
-        else 
-        {
-            return Input.GetAxis("Horizontal");
-        }
+      return Input.GetAxis("LeftJoystickX");
     }
-
-    private float GetYAxis() 
+    else
     {
-        if (useController) 
-        {
-            return -Input.GetAxis("LeftJoystickY");
-        } 
-        else 
-        {
-            return Input.GetAxis("Vertical");
-        }
+      return Input.GetAxis("Horizontal");
     }
+  }
 
-    private bool GetJump()
+  private float GetYAxis()
+  {
+    if (useController)
     {
-        if (useController)
-        {
-            return Input.GetButtonDown("A");
-        } 
-        else 
-        {
-            return Input.GetKeyDown(KeyCode.Space);
-        }
+      return -Input.GetAxis("LeftJoystickY");
     }
-
-    private Vector3 CameraRelativeFlatten(Vector3 input, Vector3 localUp)
+    else
     {
-        // If this script is on your camera object, you can use this.transform instead.
-
-        // The first part creates a rotation looking into the ground, with
-        // "up" matching the camera's look direction as closely as it can. 
-        // The second part rotates this 90 degrees, so "forward" input matches 
-        // the camera's look direction as closely as it can in the horizontal plane.
-        Quaternion flatten = Quaternion.LookRotation(
-                                            -localUp, 
-                                            this.cam.forward
-                                    )
-                                        * Quaternion.Euler(Vector3.right * -90f);
-
-        // Now we rotate our input vector into this frame of reference
-        return flatten * input;
+      return Input.GetAxis("Vertical");
     }
+  }
 
-
-    // Hit an object over
-    void OnControllerColliderHit(ControllerColliderHit hit)
+  private bool GetJump()
+  {
+    if (useController)
     {
-        Rigidbody body = hit.collider.attachedRigidbody;
-
-        // no rigidbody
-        if (body == null || body.isKinematic)
-        {
-            return;
-        }
-
-        // Break break-able objects
-        Knockable knockable = hit.gameObject.GetComponent("Knockable") as Knockable;
-        if (knockable != null) {
-            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-            Vector3 pushForce = pushDir * pushPower;
-            knockable.trigger(pushForce);
-        }
+      return Input.GetButtonDown("A");
     }
-
-
-    // TODO: delete following code
-    public int GetFood()
+    else
     {
-        return food;
+      return Input.GetKeyDown(KeyCode.Space);
     }
+  }
 
-    public int GetMaxFood()
+  private Vector3 CameraRelativeFlatten(Vector3 input, Vector3 localUp)
+  {
+    // If this script is on your camera object, you can use this.transform instead.
+
+    // The first part creates a rotation looking into the ground, with
+    // "up" matching the camera's look direction as closely as it can. 
+    // The second part rotates this 90 degrees, so "forward" input matches 
+    // the camera's look direction as closely as it can in the horizontal plane.
+    Quaternion flatten = Quaternion.LookRotation(-localUp,
+        this.cam.forward
+      ) *
+      Quaternion.Euler(Vector3.right * -90f);
+
+    // Now we rotate our input vector into this frame of reference
+    return flatten * input;
+  }
+
+  // Hit an object over
+  void OnControllerColliderHit(ControllerColliderHit hit)
+  {
+    Rigidbody body = hit.collider.attachedRigidbody;
+
+    // no rigidbody
+    if (body == null || body.isKinematic)
     {
-        return maxFood;
+      return;
     }
 
-    public bool IncreaseFood()
+    // Break break-able objects
+    Knockable knockable = hit.gameObject.GetComponent("Knockable") as Knockable;
+    if (knockable != null)
     {
-        if (food >= maxFood)
-        {
-            Debug.Log("At max food");
-            return false;
-        }
-
-        Debug.Log("Got a food");
-        food++;
-        AudioManager.instance.Play("ItemCollect");
-        return true;
+      Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+      Vector3 pushForce = pushDir * pushPower;
+      knockable.trigger(pushForce);
     }
+  }
 
-    public bool DecreaseFood()
+  // TODO: delete following code
+  public int GetFood()
+  {
+    return food;
+  }
+
+  public int GetMaxFood()
+  {
+    return maxFood;
+  }
+
+  public bool IncreaseFood()
+  {
+    if (food >= maxFood)
     {
-        if (food <= 0)
-        {
-            Debug.Log("Have no food");
-            return false;
-        }
-
-        Debug.Log("Gave a food");
-        food--;
-        return true;
+      Debug.Log("At max food");
+      return false;
     }
+
+    Debug.Log("Got a food");
+    food++;
+    AudioManager.instance.Play("ItemCollect");
+    return true;
+  }
+
+  public bool DecreaseFood()
+  {
+    if (food <= 0)
+    {
+      Debug.Log("Have no food");
+      return false;
+    }
+
+    Debug.Log("Gave a food");
+    food--;
+    return true;
+  }
 }
