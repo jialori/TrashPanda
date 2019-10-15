95a62b13ef47d0a7c3a0ffc283a8da366f30bafc
Gabriel <neoco@techie.com>
2019-10-15 18:33:30 -0400

    Added more content to the Options menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
     public string sceneName;
+    
 
     public void LaunchGame()
     {
