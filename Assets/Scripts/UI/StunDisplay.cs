984da7c293b3547fde425fc3e1504f0acea1fa96
Gabriel <neoco@techie.com>
2019-10-29 15:56:22 -0400

    Added stun effect to UI

---
+++ Assets/Scripts/UI/StunDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StunDisplay : MonoBehaviour
+{
+    [SerializeField] private Text stunText;
+    [SerializeField] RaccoonController raccoon;
+
+    void Start()
+    {
+        //raccoon = GameObject.Find("Raccoon").GetComponent<RaccoonController>();
+    }
+
+    void Update()
+    {
+        if (raccoon.isStunned)
+        {
+            stunText.enabled = true;
+        }
+        else
+        {
+            stunText.enabled = false;
+        }
+    }
+}
