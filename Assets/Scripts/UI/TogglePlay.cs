0e568293010484aa626b6499c1e2ed0b5938e609
jialori <lori.jia@mail.utoronto.ca>
2019-10-30 16:40:44 -0400

    Added buttons to Pause menu

--- Assets/Scripts/UI/TogglePlay.cs
+++ Assets/Scripts/UI/TogglePlay.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class TogglePlay : MonoBehaviour
 {
 
     public GameObject pauseMenu;
+    public EventSystem es;
+
+    void Start()
+    {
+        es = GetComponent<EventSystem>();
+    }
 
     void Update()
     {
-        if (GetTogglePlay())
+        bool _validInput = false;
+        if (GetTogglePlay() ||
+            (GetA() && pauseMenu.activeSelf && es?.currentSelectedGameObject?.GetComponent<Button>()))
+        {
+            _validInput = true;
+        }
+
+        if (_validInput)
         {
             GameManager.instance.TogglePlay();
             pauseMenu.SetActive(!pauseMenu.activeSelf);
@@ -25,6 +40,18 @@ public class TogglePlay : MonoBehaviour
         else
         {
             return Input.GetKeyDown(KeyCode.Escape);
+        }
+    }
+
+    private bool GetA()
+    {
+        if (GameManager.instance.UseController)
+        {
+            return Input.GetButtonDown("A");
+        }
+        else
+        {
+            return Input.GetKeyDown(KeyCode.Space);
         }
     }
 }
