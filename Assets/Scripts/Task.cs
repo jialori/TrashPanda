371612aa0669c10d65cf8043b1df78964bee8331
Gabriel <neoco@techie.com>
2019-11-11 20:36:06 -0500

    Added DestroyObjectsOnFloor task

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    protected string description;
+
+    public abstract bool isComplete();
+
+    //protected abstract void updateProgress();
+}
