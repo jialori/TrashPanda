5905c54d08c949a81750d5efffdf35dff4febe57
jialori <lori.jia@mail.utoronto.ca>
2019-11-19 12:40:49 -0500

    Implemented auto-scrolling for Objective Completed Display in the End scene

--- Assets/Scripts/UI/ObjectiveFinalShowcase.cs
+++ Assets/Scripts/UI/ObjectiveFinalShowcase.cs
@@ -18,13 +18,29 @@ public class ObjectiveFinalShowcase : MonoBehaviour
     IEnumerator AutoScrollObjectiveList()
     {
         List<GameTask> completedTasks = TaskManager.instance.GetCompletedTasks();
-        Debug.Log(completedTasks.Count);
-        int length = Mathf.Min(completedTasks.Count, 3);
-        for (int i=0; i < length; i++) 
+        // Debug.Log("total count:" + completedTasks.Count);
+        int count = Mathf.Min(completedTasks.Count, 3);
+
+        for (int i=0; i < count; i++) 
         {
-        	Debug.Log(i);
 	        objectiveDisplays[i].text = completedTasks[i].description;
+	        yield return new WaitForSeconds(.3f);
         }
-        yield return new WaitForSeconds(3);
+
+        if (count < 3)
+        {
+        	yield break;
+        }
+
+        for (int i=0; i < completedTasks.Count - count; i++) 
+        {
+        	for (int j = 0; j < 3; j++)
+        	{
+		        // Debug.Log("current displaying as first :" + (i + 1 + j + 1));
+		        objectiveDisplays[j].text = completedTasks[i + 1 + j].description;
+        	}
+	        yield return new WaitForSeconds(.3f);        		
+        }
+
     }
 }
