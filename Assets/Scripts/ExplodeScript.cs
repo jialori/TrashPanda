1aa1c115508996e6465f0ac13e0e2ac9f373f633
Mason Victoria <masonachu@gmail.com>
2019-11-27 12:10:41 -0500

    breakable audio added

--- Assets/Scripts/ExplodeScript.cs
+++ Assets/Scripts/ExplodeScript.cs
@@ -7,15 +7,19 @@ public class ExplodeScript : MonoBehaviour
     public bool startExplosion = false;
     public float destroyDelay = 5;
 
+    public AudioSource SFX;
+    public AudioClip BreakSFX;
 
     // Update is called once per frame
     public void Explode(Vector3 momentum)
     {
+        GetComponent<AudioSource>();
         foreach (Transform t in transform)
         {
             Rigidbody rb = t.GetComponent<Rigidbody>();
             if (rb != null)
             {
+                SFX.PlayOneShot(BreakSFX, 0.5F);
                 rb.AddForce(momentum/rb.mass, ForceMode.Impulse);
             }
             Destroy(t.gameObject, destroyDelay);
